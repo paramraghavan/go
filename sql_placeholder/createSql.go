010bf6a09f6f3734b2c72f1a6b5b223564150714
Param Raghavan <52529498+paramraghavan@users.noreply.github.com>
2023-09-15 09:21:41 -0400

    Update createSql.go

--- sql_placeholder/createSql.go
+++ sql_placeholder/createSql.go
@@ -5,6 +5,11 @@ import "fmt"
 // placeholder for sql
 func main() {
 	where := "col1='a' and col2='b'"
+	// in multiple line with backticks
+	where_mulitple_lines = `col1='a'
+ 				and 
+     				col2='b' `
+	
 	groupBy := "group by co1, col2"
 
 	str := "select * from table1 where %s group by %s"
